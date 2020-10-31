fba8b94b7a63a2ed2fc168cd54e8868ccf2e1f53
GabrielaRamos23 <gabrielaramosch23@gmail.com>
2020-10-31 11:02:22 -0500

    password - alert

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<form action="validacion.php" method="POST" accept-charset="utf-8">
+		<input type="number" name="dni" value="" placeholder="">
+		<input type="submit" name="" value="CONSULTAR">
+	</form>
+	
+</body>
+</html>
